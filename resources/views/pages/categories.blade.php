f917f959b9e409e073ed4147f05140cb238d7520
JamesFidelis <jamesfidelis29@gmail.com>
2023-07-21 17:25:10 +0300

    Learning Laravel Basics

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -26,7 +26,7 @@
                     @if(session('success'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         <p>{{session('success')}}.</p>
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"/>
 
                     </div>
                     @endif
